68583a3e4078184aa0067adedf3bc50da4695a4a
Maurício Benigno <mauriciobenigno@hotmail.com>
2019-12-28 15:23:12 -0300

    1 - Criação dos Endpoints e API para requisições GET

--- maxApp/app/src/main/java/br/com/mauriciobenigno/maxapp/api/ApiService.kt
+++ maxApp/app/src/main/java/br/com/mauriciobenigno/maxapp/api/ApiService.kt
@@ -1,4 +1,21 @@
 package br.com.mauriciobenigno.maxapp.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object ApiService {
+
+    private var URL = "https://api.myjson.com/bins/"
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getEndPoints() : EndPoints{
+        return getRetrofit().create(EndPoints::class.java)
+    }
 }
---
+++ maxApp/app/src/main/java/br/com/mauriciobenigno/maxapp/api/EndPoints.kt
@@ -0,0 +1,13 @@
+package br.com.mauriciobenigno.maxapp.api
+
+import br.com.mauriciobenigno.maxapp.models.pedido
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface EndPoints {
+    //@GET("1bo7qj")
+    //fun getClient() : Call<cliente>
+
+    @GET("wjl97")
+    fun getAllRequests() : Call<List<pedido>>
+}
